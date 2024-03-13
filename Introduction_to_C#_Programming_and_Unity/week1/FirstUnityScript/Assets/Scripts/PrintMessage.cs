69e3a6104327891cabef0eece30cc28c74df5d10
Hümeyra Bali <46250327+HumeyraBali@users.noreply.github.com>
2024-03-13 17:20:02 +0300

    Add files via upload

---
+++ Introduction_to_C#_Programming_and_Unity/week1/FirstUnityScript/Assets/Scripts/PrintMessage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Prints a message
+/// </summary>
+public class PrintMessage : MonoBehaviour
+{
+    /// <summary>
+	/// Start is called before the first frame update
+	/// </summary>	
+    void Start()
+    {
+        Debug.Log("Hi, noob!");
+    }
+}
