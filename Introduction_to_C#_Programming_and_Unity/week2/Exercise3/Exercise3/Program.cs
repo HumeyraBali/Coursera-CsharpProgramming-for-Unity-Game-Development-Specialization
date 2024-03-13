84f7cb8eae2efdfe9145eaae18cb1f4a72599d37
Hümeyra Bali <46250327+HumeyraBali@users.noreply.github.com>
2024-03-13 17:24:01 +0300

    Add files via upload

---
+++ Introduction_to_C#_Programming_and_Unity/week2/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise3
+{
+    /// <summary>
+    /// Exercise 3 Solution
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Practice declaring, assigning, and printing a variable
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // declare, assign, and print a variable
+            int age;
+            age = 42;
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
