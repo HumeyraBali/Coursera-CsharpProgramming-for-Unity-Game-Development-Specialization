7537166ea38f56dc53fd33a780faebbb14d57571
Hümeyra Bali <46250327+HumeyraBali@users.noreply.github.com>
2024-03-13 17:29:47 +0300

    Add files via upload

---
+++ Introduction_to_C#_Programming_and_Unity/week4/Unity2DBasics/Assets/Scripts/Talker.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Prints a message
+/// </summary>
+public class Talker : MonoBehaviour
+{
+    /// <summary>
+	/// Start is called before the first frame update
+	/// </summary>	
+    void Start()
+    {
+        // say something
+        Debug.Log("I'm the best game object ever!");
+    }
+}
